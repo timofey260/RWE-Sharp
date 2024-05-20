024b29ae3406b3411f4446751e81d8c9ed08f107
timofey <smailovtimofey@gmail.com>
2024-05-20 23:13:44 +0300

    new thingies

--- core/Modify/ConfigModule.py
+++ core/Modify/ConfigModule.py
@@ -1,3 +1,20 @@
+from PySide6.QtWidgets import QCheckBox
+from PySide6.QtCore import Slot, Signal
+
+class ConfigObject:
+    def __init__(self, path, object, default):
+        self.path = path
+        self.object = object
+        self.default = default
+        self.value = default
+
+    def load_value(self):
+        ...
+
+    def save_value(self):
+        ...
+
+
 class ConfigModule:
     """
     Base for creating custom mod configs
@@ -7,8 +24,16 @@ class ConfigModule:
     def __init__(self, mod):
         self.mod = mod
         self.manager = mod.manager
+        self.values: dict[str, ConfigObject] = {}
 
     def register_value(self, path, value) -> None:
         """
         registers value to be saved
         """
+
+    def register_widgets(self):
+        ...
+
+    def register_checkbox(self, path, object: QCheckBox, default):
+        self.values[path] = ConfigObject(path, object, default)
+        object.stateChanged.connect()
