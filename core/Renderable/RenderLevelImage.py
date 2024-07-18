4f4724962b54f71b7d03782ef66eb98254415ac1
timofey <smailovtimofey@gmail.com>
2024-07-18 22:24:56 +0300

    bullshit doesn't work im angy

--- core/Renderable/RenderLevelImage.py
+++ core/Renderable/RenderLevelImage.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import QSize
-from PySide6.QtGui import QPixmap, QPainter
+from PySide6.QtGui import QPixmap, QPainter, QImage
 from core.Renderable.RenderImage import RenderImage
 from core.info import CELLSIZE
 
@@ -9,8 +9,9 @@ class RenderLevelImage(RenderImage):
         super().__init__(mod, depth, QSize(mod.manager.level_width * CELLSIZE, mod.manager.level_height * CELLSIZE))
 
     def level_resized(self):
-        self.image = QPixmap(QSize(self.mod.manager.level_width * CELLSIZE, self.mod.manager.level_height * CELLSIZE))
-        self.painter = None
-        self.painter = QPainter(self.image)
+        # self.image = QPixmap(QSize(self.mod.manager.level_width * CELLSIZE, self.mod.manager.level_height * CELLSIZE))
+        # self.painter = None
+        # self.painter = QPainter(self.image)
+        # self.painter.begin(self.image)
         self.renderedtexture.setPixmap(self.image)
         self.redraw()
