bdb7e892df5e58e87eb0860fe19c26fe8cbd1f8f
ATOM <116904039+ATOM1138@users.noreply.github.com>
2024-08-19 17:13:00 +0200

    awd;l,as]pp]dja[]psodk as\[

--- BaseMod/palettes/CustomQSS.py
+++ BaseMod/palettes/CustomQSS.py
@@ -1,12 +1,15 @@
 import os
 import re
-
+from BaseMod.Palettes.RaspberryDark import RaspberryDark
 from PySide6.QtWidgets import QApplication
 from RWESharp.Core import PATH_BASEMOD
 from RWESharp.Modify import Theme
 
-folder_path = os.path.abspath("baseMod\Palettes\qssfiles")
-current_theme = "sharp"
+with open(r"C:\Users\ATOM\Documents\GitHub\RWE-Sharp\BaseMod\Palettes\qssfiles\sharp.txt") as file:
+    current_theme = file.read()
+
+color_map = RaspberryDark
+
 
 class ThemeManager(Theme):
     def __init__(self, mod, widget=None):
@@ -22,85 +25,83 @@ class ThemeManager(Theme):
                 print("Error: QApplication instance not found.")
 
     def get_stylesheet(self):
-        #with open(r"C:\Users\ATOM\Documents\GitHub\RWE-Sharp\BaseMod\Palettes\qssfiles\sharp.txt") as file:
+        # with open(r"C:\Users\ATOM\Documents\GitHub\RWE-Sharp\BaseMod\Palettes\qssfiles\sharp.txt") as file:
         # TODO, not thigs
-            return CSSPaletizer("palette5.png", )
-
+        print(CSSPaletizer(color_map, current_theme))
+        return CSSPaletizer(color_map, current_theme)
+placeholder_mapping = { }
 
 def CSSPaletizer(palette_name, input_string):
-    placeholder_mapping = {
-        # base colors
-        "{{base_dark}}": 0,
-        "{{base_medium}}": 1,
-        "{{base_light}}": 2,
-        "{{text_misc}}": 3,
-        # border
-        "{{border_dark}}": 4,
-        "{{border_medium}}": 5,
-        "{{border_light}}": 6,
-        # text
-        "{{text_dark}}": 7,
-        "{{text_medium}}": 8,
-        "{{text_light}}": 9,
-        "{{text_disabled}}": 10,
-        "{{text_enabled}}": 11,
-        # accents
-        "{{accent_light}}": 12,
-        "{{accent_medium}}": 13,
-        "{{accent_dark}}": 14,
-        # color variations
-        "{{alt_base_dark}}": 15,
-        "{{alt_base_medium}}": 16,
-        "{{alt_base_light}}": 17,
-        "{{alt_text_misc}}": 18,
-        "{{alt_border_dark}}": 19,
-        "{{alt_border_medium}}": 20,
-        "{{alt_border_light}}": 21,
-        "{{alt_text_dark}}": 22,
-        "{{alt_text_medium}}": 23,
-        "{{alt_text_light}}": 24,
-        "{{alt_text_disabled}}": 25,
-        "{{alt_text_enabled}}": 26,
-        "{{alt_accent_light}}": 27,
-        "{{alt_accent_medium}}": 28,
-        "{{alt_accent_dark}}": 29,
-        # specific colors
-        "{{misc_color_30}}": 30,
-        "{{misc_color_31}}": 31,
-        "{{misc_color_32}}": 32,
-        "{{misc_color_33}}": 33,
-        "{{misc_color_34}}": 34,
-        "{{misc_color_35}}": 35,
-        "{{misc_color_36}}": 36,
-        "{{misc_color_37}}": 37,
-        "{{misc_color_38}}": 38,
-        "{{misc_color_39}}": 39,
-        "{{misc_color_40}}": 40,
-        "{{misc_color_41}}": 41,
-        "{{misc_color_42}}": 42,
-        "{{misc_color_43}}": 43,
-        "{{misc_color_44}}": 44,
-        "{{misc_color_45}}": 45,
-        "{{misc_color_46}}": 46,
-        "{{misc_color_47}}": 47,
-        "{{misc_color_48}}": 48,
-        "{{misc_color_49}}": 49,
-        "{{misc_color_50}}": 50,
-        "{{misc_color_51}}": 51,
-        "{{misc_color_52}}": 52,
-        "{{misc_color_53}}": 53,
-        "{{misc_color_54}}": 54,
-        "{{misc_color_55}}": 55,
-        "{{misc_color_56}}": 56,
-        "{{misc_color_57}}": 57,
-        "{{misc_color_58}}": 58,
-        "{{misc_color_59}}": 59
+      placeholder_mapping = {
+           # base colors
+           "@base_dark": 0,
+           "@base_medium": 1,
+           "@base_light": 2,
+           "@text_misc": 3,
+           # border
+           "@border_dark": 4,
+           "@border_medium": 5,
+           "@border_light": 6,
+           # text
+           "@text_dark": 7,
+           "@text_medium": 8,
+           "@text_light": 9,
+           "@text_disabled": 10,
+           "@text_enabled": 11,
+           # accents
+           "@accent_light": 12,
+           "@accent_medium": 13,
+           "@accent_dark": 14,
+           # color variations
+           "@alt_base_dark": 15,
+           "@alt_base_medium": 16,
+           "@alt_base_light": 17,
+           "@alt_text_misc": 18,
+           "@alt_border_dark": 19,
+           "@alt_border_medium": 20,
+           "@alt_border_light": 21,
+           "@alt_text_dark": 22,
+           "@alt_text_medium": 23,
+           "@alt_text_light": 24,
+           "@alt_text_disabled": 25,
+           "@alt_text_enabled": 26,
+           "@alt_accent_light": 27,
+           "@alt_accent_medium": 28,
+           "@alt_accent_dark": 29,
+           # specific colors
+           "@misc_color_30": 30,
+           "@misc_color_31": 31,
+           "@misc_color_32": 32,
+           "@misc_color_33": 33,
+           "@misc_color_34": 34,
+           "@misc_color_35": 35,
+           "@misc_color_36": 36,
+           "@misc_color_37": 37,
+           "@misc_color_38": 38,
+           "@misc_color_39": 39,
+           "@misc_color_40": 40,
+           "@misc_color_41": 41,
+           "@misc_color_42": 42,
+           "@misc_color_43": 43,
+           "@misc_color_44": 44,
+           "@misc_color_45": 45,
+           "@misc_color_46": 46,
+           "@misc_color_47": 47,
+           "@misc_color_48": 48,
+           "@misc_color_49": 49,
+           "@misc_color_50": 50,
+           "@misc_color_51": 51,
+           "@misc_color_52": 52,
+           "@misc_color_53": 53,
+           "@misc_color_54": 54,
+           "@misc_color_55": 55,
+           "@misc_color_56": 56,
+           "@misc_color_57": 57,
+           "@misc_color_58": 58,
+           "@misc_color_59": 59
 
-    }
-    # Define a fixed mapping of CSS placeholders to palette colors
-
-    # Parse the palette image and get the colors in the order of the mapping
-    color_map = {"0": "#333333", "1": "#000000", "2": "#1f1f1f", "13": "#8B4FA0",}
+       }
+       # Define a fixed mapping of CSS placeholders to palette colors
 
     # Create a dictionary to map each placeholder to its corresponding color
     color_replacements = {key: color_map.get(str(idx)) for key, idx in placeholder_mapping.items() if idx is not None}
@@ -110,5 +111,5 @@ def CSSPaletizer(palette_name, input_string):
         placeholder = match.group(0)
         return color_replacements.get(placeholder, placeholder)
 
-    placeholder_pattern = r'{{\w+}}'
+    placeholder_pattern = r'@\w+@'
     return re.sub(placeholder_pattern, replace_color, input_string)
--- BaseMod/palettes/RaspberryDark.py
+++ BaseMod/palettes/RaspberryDark.py
@@ -1,8 +1,12 @@
+from PySide6.QtGui import QColor
+
 from RWESharp.Modify import Theme
 from RWESharp.Configurable import IntConfigurable
 from RWESharp.Core import PATH_BASEMOD
 import re
 import os
+
+from core.configTypes.QtTypes import ColorConfigurable
 
 
 class RaspberryDark(Theme):
@@ -10,9 +14,80 @@ class RaspberryDark(Theme):
         super().__init__("Raspberry Dark", mod)
         self.styleindex = IntConfigurable(mod, "rpdar.styleoption", 0, description="Style file")
 
+        self.colors = [
+            # base colors
+            ColorConfigurable(mod, "@base_dark", QColor.fromString("#000000"), "Base dark color"),
+            ColorConfigurable(mod, "@base_medium", QColor.fromString("#333333"), "Base medium color"),
+            ColorConfigurable(mod, "@base_light", QColor.fromString("#666666"), "Base light color"),
+
+
+            # border colors
+            ColorConfigurable(mod, "@border_dark", QColor.fromString("#000000"), "Border dark color"),
+            ColorConfigurable(mod, "@border_medium", QColor.fromString("#333333"), "Border medium color"),
+            ColorConfigurable(mod, "@border_light", QColor.fromString("#666666"), "Border light color"),
+            # text colors
+            ColorConfigurable(mod, "@text_dark", QColor.fromString("#000000"), "Text dark color"),
+            ColorConfigurable(mod, "@text_medium", QColor.fromString("#333333"), "Text medium color"),
+            ColorConfigurable(mod, "@text_light", QColor.fromString("#666666"), "Text light color"),
+            ColorConfigurable(mod, "@text_disabled", QColor.fromString("#999999"), "Text disabled color"),
+            ColorConfigurable(mod, "@text_enabled", QColor.fromString("#FFFFFF"), "Text enabled color"),
+            # accent colors
+            ColorConfigurable(mod, "@accent_light", QColor.fromString("#FFCCCC"), "Accent light color"),
+            ColorConfigurable(mod, "@accent_medium", QColor.fromString("#FF6666"), "Accent medium color"),
+            ColorConfigurable(mod, "@accent_dark", QColor.fromString("#CC0000"), "Accent dark color"),
+            ColorConfigurable(mod, "@alt_base_dark", QColor.fromString("#0A0A0A"), "Alternative base dark color"),
+            ColorConfigurable(mod, "@alt_base_medium", QColor.fromString("#3A3A3A"), "Alternative base medium color"),
+            ColorConfigurable(mod, "@alt_base_light", QColor.fromString("#6A6A6A"), "Alternative base light color"),
+            ColorConfigurable(mod, "@alt_text_misc", QColor.fromString("#9A9A9A"),
+                              "Alternative miscellaneous text color"),
+            ColorConfigurable(mod, "@alt_border_dark", QColor.fromString("#0A0A0A"), "Alternative border dark color"),
+            ColorConfigurable(mod, "@alt_border_medium", QColor.fromString("#3A3A3A"),
+                              "Alternative border medium color"),
+            ColorConfigurable(mod, "@alt_border_light", QColor.fromString("#6A6A6A"), "Alternative border light color"),
+            ColorConfigurable(mod, "@alt_text_dark", QColor.fromString("#0A0A0A"), "Alternative text dark color"),
+            ColorConfigurable(mod, "@alt_text_medium", QColor.fromString("#3A3A3A"), "Alternative text medium color"),
+            ColorConfigurable(mod, "@alt_text_light", QColor.fromString("#6A6A6A"), "Alternative text light color"),
+            ColorConfigurable(mod, "@alt_text_disabled", QColor.fromString("#9A9A9A"),
+                              "Alternative text disabled color"),
+            ColorConfigurable(mod, "@alt_text_enabled", QColor.fromString("#FAFAFA"), "Alternative text enabled color"),
+            ColorConfigurable(mod, "@alt_accent_light", QColor.fromString("#FFAAAA"), "Alternative accent light color"),
+            ColorConfigurable(mod, "@alt_accent_medium", QColor.fromString("#FF5555"),
+                              "Alternative accent medium color"),
+            ColorConfigurable(mod, "@alt_accent_dark", QColor.fromString("#AA0000"), "Alternative accent dark color"),
+            ColorConfigurable(mod, "@misc_color_30", QColor.fromString("#123456"), "Miscellaneous color 30"),
+            ColorConfigurable(mod, "@misc_color_31", QColor.fromString("#654321"), "Miscellaneous color 31"),
+            ColorConfigurable(mod, "@misc_color_32", QColor.fromString("#abcdef"), "Miscellaneous color 32"),
+            ColorConfigurable(mod, "@misc_color_33", QColor.fromString("#fedcba"), "Miscellaneous color 33"),
+            ColorConfigurable(mod, "@misc_color_34", QColor.fromString("#112233"), "Miscellaneous color 34"),
+            ColorConfigurable(mod, "@misc_color_35", QColor.fromString("#334455"), "Miscellaneous color 35"),
+            ColorConfigurable(mod, "@misc_color_36", QColor.fromString("#556677"), "Miscellaneous color 36"),
+            ColorConfigurable(mod, "@misc_color_37", QColor.fromString("#778899"), "Miscellaneous color 37"),
+            ColorConfigurable(mod, "@misc_color_38", QColor.fromString("#99aabb"), "Miscellaneous color 38"),
+            ColorConfigurable(mod, "@misc_color_39", QColor.fromString("#bbaacc"), "Miscellaneous color 39"),
+            ColorConfigurable(mod, "@misc_color_40", QColor.fromString("#ccbbdd"), "Miscellaneous color 40"),
+            ColorConfigurable(mod, "@misc_color_41", QColor.fromString("#ddeeff"), "Miscellaneous color 41"),
+            ColorConfigurable(mod, "@misc_color_42", QColor.fromString("#ffccaa"), "Miscellaneous color 42"),
+            ColorConfigurable(mod, "@misc_color_43", QColor.fromString("#aabbcc"), "Miscellaneous color 43"),
+            ColorConfigurable(mod, "@misc_color_44", QColor.fromString("#cceeff"), "Miscellaneous color 44"),
+            ColorConfigurable(mod, "@misc_color_45", QColor.fromString("#ffaaee"), "Miscellaneous color 45"),
+            ColorConfigurable(mod, "@misc_color_46", QColor.fromString("#bbccdd"), "Miscellaneous color 46"),
+            ColorConfigurable(mod, "@misc_color_47", QColor.fromString("#ccddee"), "Miscellaneous color 47"),
+            ColorConfigurable(mod, "@misc_color_48", QColor.fromString("#ddeeff"), "Miscellaneous color 48"),
+            ColorConfigurable(mod, "@misc_color_49", QColor.fromString("#eeffaa"), "Miscellaneous color 49"),
+            ColorConfigurable(mod, "@misc_color_50", QColor.fromString("#ffddaa"), "Miscellaneous color 50"),
+            ColorConfigurable(mod, "@misc_color_51", QColor.fromString("#ffddee"), "Miscellaneous color 51"),
+            ColorConfigurable(mod, "@misc_color_52", QColor.fromString("#aaffdd"), "Miscellaneous color 52"),
+            ColorConfigurable(mod, "@misc_color_53", QColor.fromString("#bbddff"), "Miscellaneous color 53"),
+            ColorConfigurable(mod, "@misc_color_54", QColor.fromString("#ccffee"), "Miscellaneous color 54"),
+            ColorConfigurable(mod, "@misc_color_55", QColor.fromString("#ddffee"), "Miscellaneous color 55"),
+            ColorConfigurable(mod, "@misc_color_56", QColor.fromString("#aaddff"), "Miscellaneous color 56"),
+            ColorConfigurable(mod, "@misc_color_57", QColor.fromString("#bbeeaa"), "Miscellaneous color 57"),
+            ColorConfigurable(mod, "@misc_color_58", QColor.fromString("#ccbbaa"), "Miscellaneous color 58"),
+            ColorConfigurable(mod, "@misc_color_59", QColor.fromString("#ddccbb"), "Miscellaneous color 59"),
+        ]
 
     def palette_enable(self):
-        with open(os.path.join(PATH_BASEMOD, "palettes", "qssfiles", ["circular.txt", "sharp.txt", "atmoled.txt", "darkeum.txt"][self.styleindex.value])) as f:
+        with open(os.path.join(PATH_BASEMOD, "Palettes", "qssfiles", ["circular.txt", "sharp.txt", "atmoled.txt", "darkeum.txt"][self.styleindex.value])) as f:
             self.mod.manager.application.setStyleSheet(f.read())
 
     def palette_disable(self):
