1e984c0212979f076f288a905dc80f8dc2bf356e
timofey <smailovtimofey@gmail.com>
2025-07-06 23:28:18 +0300

    package manager prototype

--- core/pacman/Pacman.py
+++ core/pacman/Pacman.py
@@ -6,30 +6,40 @@ import re
 from PySide6.QtWidgets import QProgressDialog, QApplication
 from PySide6.QtCore import QThread
 
-# package manager to download mods(kinda)
+
 class LoadThread(QThread):
-    def __init__(self, dialog):
+    """
+    package manager to download mods(kinda)
+    """
+    def __init__(self, dialog: QProgressDialog, app: QApplication):
         super().__init__()
         self.dialog = dialog
+        self.app = app
 
     def run(self, /):
-        proc = subprocess.Popen([sys.executable, '-m', 'pip', 'install', "--progress-bar=raw", "-U", "pyside6"], stdout=subprocess.PIPE)
+        proc = subprocess.Popen([sys.executable, '-m', 'pip', 'install', "--progress-bar=raw", "-U", "style"], stdout=subprocess.PIPE)
         while True:
             char = proc.stdout.readline()
             if not char:
-                break
+                self.quit()
+                self.dialog.hide()
+                self.app.quit()
+                return
             string = char.decode()
             m = re.match(r"Progress (\d+) of (\d+)", string)
             if m is not None:
                 val, total = int(m.group(1)), int(m.group(2))
                 self.dialog.setValue(int(val * 100 / total))
+            else:
+                self.dialog.setLabelText(string)
             print(string, end="")
+
 
 app = QApplication()
 dialog = QProgressDialog("installing shit", "cancel", 0, 100)
-dialog.setValue(50)
-load = LoadThread(dialog)
+dialog.show()
+dialog.canceled.connect(app.quit)
+load = LoadThread(dialog, app)
 load.start()
 
-load.start()
-app.exec()
+sys.exit(app.exec())
